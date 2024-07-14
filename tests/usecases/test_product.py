2db31ec73a7dc63b6a58617661398c5c4c9fabef
Lucas Cabral <122704555+lucascamourao@users.noreply.github.com>
2024-07-13 22:03:43 -0300

    Update test_product.py

--- tests/usecases/test_product.py
+++ tests/usecases/test_product.py
@@ -2,7 +2,7 @@ from typing import List
 from uuid import UUID
 
 import pytest
-from store.core.exceptions import NotFoundException
+from store.core.exceptions import DuplicateEntryException, NotFoundException
 from store.schemas.product import ProductOut, ProductUpdateOut
 from store.usecases.product import product_usecase
 
@@ -12,6 +12,16 @@ async def test_usecases_create_should_return_success(product_in):
 
     assert isinstance(result, ProductOut)
     assert result.name == "Iphone 14 Pro Max"
+
+
+# Create: Map an exception in case an insertion error occurs and capture it in the controller
+async def test_usecases_create_should_return_duplicate_error(product_in):
+    await product_usecase.create(body=product_in)
+
+    with pytest.raises(DuplicateEntryException) as err:
+        await product_usecase.create(body=product_in)
+
+    assert err.value.message == "Product already exists"
 
 
 async def test_usecases_get_should_return_success(product_inserted):
