b9e56312b49fa8111d5b52573eaaea5996ba9892
Lucas Cabral <122704555+lucascamourao@users.noreply.github.com>
2024-07-14 01:34:33 -0300

    Update product.py

--- store/usecases/product.py
+++ store/usecases/product.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import List
 from uuid import UUID
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
@@ -15,6 +16,22 @@ class ProductUsecase:
         self.collection = self.database.get_collection("products")
 
     async def create(self, body: ProductIn) -> ProductOut:
+        # Check if a product with the same name and different price exists
+        existing_product = await self.collection.find_one(
+            {"name": body.name, "price": {"$ne": body.price}}
+        )
+        if existing_product:
+            raise DuplicateEntryException(
+                "Product with the same name but different price already exists"
+            )
+
+        product_model = ProductModel(**body.model_dump())
+        await self.collection.insert_one(product_model.model_dump())
+
+        return ProductOut(**product_model.model_dump())
+
+    """
+    async def create(self, body: ProductIn) -> ProductOut:
         product_model = ProductModel(**body.model_dump())
         # Create: Map an exception in case an insertion error occurs and capture it in the controller
         try:
@@ -23,6 +40,7 @@ class ProductUsecase:
             raise DuplicateEntryException("Product already exists! ")
 
         return ProductOut(**product_model.model_dump())
+    """
 
     async def get(self, id: UUID) -> ProductOut:
         result = await self.collection.find_one({"id": id})
@@ -32,15 +50,34 @@ class ProductUsecase:
 
         return ProductOut(**result)
 
-    async def query(self) -> List[ProductOut]:
-        return [ProductOut(**item) async for item in self.collection.find()]
+    async def query(
+        self, min_price: float = None, max_price: float = None
+    ) -> List[ProductOut]:
+        filter_query = {}
+
+        if min_price is not None and max_price is not None:
+            filter_query["price"] = {"$gt": min_price, "$lt": max_price}
+
+        # Fetch products matching the filter criteria
+        cursor = self.collection.find(filter_query)
+        products = [
+            ProductOut(**product) async for product in await cursor.to_list(length=None)
+        ]
+
+        return products
 
     async def update(self, id: UUID, body: ProductUpdate) -> ProductUpdateOut:
+        body_dict = body.model_dump(exclude_none=True)
+        body_dict["updated_at"] = datetime.utcnow()
+
         result = await self.collection.find_one_and_update(
             filter={"id": id},
-            update={"$set": body.model_dump(exclude_none=True)},
+            update={"$set": body_dict},
             return_document=pymongo.ReturnDocument.AFTER,
         )
+
+        if not result:
+            raise NotFoundException(message=f"Product not found with filter: {id}")
 
         return ProductUpdateOut(**result)
 
